4402f420e20049d5e85be6da23a8e8cbc0855058
Victoria <victorah@stud.ntnu.no>
2020-07-13 18:51:01 +0200

    Lagt til eksempelfiler

---
+++ oving1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 18:47:27 2020
+
+@author: user
+"""
+
+print('Dette er øving 1')
---
+++ plot_example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 18:45:38 2020
+
+@author: user
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1, 2, 3 ,4])
+
+y = np.array([1, 2, 3 ,4])
